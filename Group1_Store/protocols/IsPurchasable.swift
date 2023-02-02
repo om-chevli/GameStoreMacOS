b136eeb4b716910c1d0bb91101ff71708721ce1c
karanmeghani18 <karanmeghani18@gmail.com>
2023-02-02 14:34:03 -0500

    Added Protocol

---
+++ Group1_Store/protocols/IsPurchasable.swift
@@ -0,0 +1,13 @@
+//
+//  IsPurchasable.swift
+//  Group1_Store
+//
+//  Created by Karan Meghani on 2023-02-02.
+//
+
+import Foundation
+
+protocol IsPurchasable {
+    var info: String { get }
+    func printReceipt(isRefund: Bool, amount: Double)
+}
